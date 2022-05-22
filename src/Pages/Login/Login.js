eafa9a0d8c70cfd00d344083d850583b24935075
antor10655gh <creativefreelancerantor@gmail.com>
2022-05-22 23:17:36 +0600

    add social login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { Link } from "react-router-dom";
+import auth from "../../firebase.init";
 
 const Login = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+  if (user) {
+    console.log(user);
+  }
+
   return (
     <div className="pt-20">
       <div className="grid grid-cols-1 place-items-center">
@@ -8,7 +17,7 @@ const Login = () => {
           style={{
             boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
           }}
-          class="form-control w-full max-w-xs p-5"
+          class="form-control w-full max-w-md p-8"
         >
           <header>
             <h1 className="text-xl lg:text-3xl font-bold">Please Login</h1>
@@ -19,7 +28,7 @@ const Login = () => {
           <input
             type="text"
             placeholder="Enter email"
-            class="input input-bordered w-full max-w-xs mb-2"
+            class="input input-bordered w-full max-w-md mb-2"
           />
           <label class="label">
             <span class="label-text text-lg">Password</span>
@@ -27,17 +36,29 @@ const Login = () => {
           <input
             type="password"
             placeholder="Enter password"
-            class="input input-bordered w-full max-w-xs"
+            class="input input-bordered w-full max-w-md"
           />
-          <label class="label pb-5">
+          <label class="label mb-5">
             <button className="text-secondary text-sm">
               Forgot Password ?
             </button>
           </label>
-          <button class="btn btn-wide w-full">Login</button>
+          <button class="btn btn-wide w-full mb-2">Login</button>
+          <span className="text-center">
+            Don't Have an Account ?{" "}
+            <Link to="/signup" className="text-secondary">
+              Create an account
+            </Link>
+          </span>
           <div class="flex flex-col w-full border-opacity-50">
             <div class="divider">OR</div>
           </div>
+          <button
+            onClick={() => signInWithGoogle()}
+            class="btn btn-outline btn-wide w-full"
+          >
+            Continue With Google
+          </button>
         </div>
       </div>
     </div>
