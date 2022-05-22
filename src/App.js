e8a92eb3f6bebeab9f4f72f2a2c9d74e9b861a73
antor10655gh <creativefreelancerantor@gmail.com>
2022-05-22 12:42:50 +0600

    install tailwind and also add daisyui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Blog from "./Pages/Blog/Blog";
+import BusinessSummary from "./Pages/BusinessSummary/BusinessSummary";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Reviews from "./Pages/Reviews/Reviews";
+import Navbar from "./Pages/Shared/Navbar";
+import SignUp from "./Pages/SignUp/SignUp";
+import Tools from "./Pages/Tools/Tools";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="lg:px-12">
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/tools" element={<Tools></Tools>}></Route>
+        <Route path="/reviews" element={<Reviews></Reviews>}></Route>
+        <Route path="/blog" element={<Blog></Blog>}></Route>
+        <Route
+          path="/businessSummary"
+          element={<BusinessSummary></BusinessSummary>}
+        ></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signup" element={<SignUp></SignUp>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        tooltrextheme: {
+          primary: "#FFAE22",
+          secondary: "#EBECF0",
+          accent: "#EBECF0",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+};
