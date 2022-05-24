724cdb2da1fb6e5df6f37b591e5a371c7298dd94
antor10655gh <creativefreelancerantor@gmail.com>
2022-05-24 13:11:27 +0600

    validaton for create admin

--- src/Pages/Dashboard/User.js
+++ src/Pages/Dashboard/User.js
@@ -1,14 +1,39 @@
 import React from "react";
+import { toast } from "react-toastify";
 
 const User = ({ user, index, refetch }) => {
   const { email, role } = user;
+  const createAdmin = () => {
+    fetch(`http://localhost:5000/user/admin/${email}`, {
+      method: "PUT",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((res) => {
+        if (res.status === 403) {
+          toast.error("Failed to Make an Admin");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          refetch();
+          toast("Make Admin Successfully", {
+            autoClose: 1000,
+          });
+        }
+      });
+  };
   return (
     <tr>
       <th>{index + 1}</th>
       <td>{email}</td>
       <td>
         {role !== "admin" ? (
-          <button class="btn btn-xs">Make Admin</button>
+          <button onClick={createAdmin} class="btn btn-xs">
+            Create Admin
+          </button>
         ) : (
           <button disabled class="btn btn-xs">
             Admin
